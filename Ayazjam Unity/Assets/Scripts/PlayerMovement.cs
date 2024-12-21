6585c1bdc792e9bd90b76e6f2f9035b431c99d68
Tahakcy <5greceptahaakcay@gmail.com>
2024-12-21 15:17:14 +0300

    player movement ve animation

--- Ayazjam Unity/Assets/Scripts/PlayerMovement.cs
+++ Ayazjam Unity/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static InputControls;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,20 +14,27 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 moveInput;
     public float gridSize = 1f;
     private Vector3 targetPosition;
+    private bool canMove = true;
     private bool isMoving = false;
     public float gridWidth = 8f;
     public float gridHeight = 8f;
 
-    public float currentAttackTimer;
+    private Animator animator;
+    public Rigidbody rb;
 
-    private InputControls inputActions;
+    public float currentAttackTimer;
+    private InputControls inputActions; // Yeni Input Sistemi
 
     private void Awake()
     {
         inputActions = new InputControls();
 
-        inputActions.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        inputActions.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+        // Hareket girdisini al
+        inputActions.Player.Movement.performed += ctx =>
+        {
+            Vector2 moveInput = ctx.ReadValue<Vector2>();
+            Move(moveInput);
+        };
     }
 
     private void OnEnable()
@@ -43,64 +49,44 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+        // Baþlangýç pozisyonunu ayarla
         targetPosition = transform.position;
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (!isMoving)
+        // Karakteri hedef pozisyona doðru hareket ettir
+        if (isMoving)
         {
-            HandleInput();
-        }
-
-        MoveToTarget();
-    }
-
-    public void Attack()
-    {
-        Debug.Log("Attack");
-    }
-
-    public void Die()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void TakeDamage(int damage)
-    {
-        throw new System.NotImplementedException();
-
-    }
-    public void HandleInput()
-    {
-        Vector3 newPosition = targetPosition;
-
-        if (moveInput.y > 0)
-        {
-            newPosition += new Vector3(0, 0, gridSize);
-        }
-        else if (moveInput.y < 0) 
-        {
-            newPosition += new Vector3(0, 0, -gridSize);
-        }
-        else if (moveInput.x < 0)
-        {
-            newPosition += new Vector3(-gridSize, 0, 0);
-        }
-        else if (moveInput.x > 0) 
-        {
-            newPosition += new Vector3(gridSize, 0, 0);
-        }
-    }
-
-    public void MoveToTarget()
-    {
-        if (transform.position != targetPosition)
-        {
-            isMoving = true;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            // Hedef pozisyona ulaþýldýðýnda hareketi durdur
+            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+            {
+                transform.position = targetPosition; // Pozisyonu tam olarak hizala
+                isMoving = false; // Hareket tamamlandý
+
+                // Hareket tamamlandýðýnda animasyonu durdur (Idle'a geç)
+                animator.SetBool("IsMoving", false);
+            }
         }
-        else { isMoving = false; }
+    }
+
+    private void Move(Vector2 moveInput)
+    {
+        // Hareket yönünü belirle
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y);
+
+        // Yeni hedef pozisyonu hesapla
+        targetPosition += direction * gridSize;
+
+        // Hareketi baþlat
+        isMoving = true;
+
+        animator.SetFloat("MoveX", moveInput.x);
+
+
+
     }
 }
